bbb1e973ef827ab239f06a0ce8f49c84c939899e
tokkaiiii <luv_3_u@naver.com>
2024-11-20 17:52:32 +0900

    :sparkles:feat(store): article 수정 기능 추가중
    - article 기능 구현
      - 수정 기능 추가중

--- src/components/ArticleEditForm.svelte
+++ src/components/ArticleEditForm.svelte
@@ -1,14 +1,26 @@
+<script>
+    export let article
+
+    let articleValue = {
+        id: article.id,
+        userEmail: article.userEmail,
+        createdAt: article.createdAt,
+        content: article.content
+    }
+</script>
 <!-- slog-content-edit-form start-->
 <div class="slog-content-box" >
     <div class="content-box-header">
         <div class="content-box-header-inner-left" >
-            <p class="p-user" >freeseamew</p>
-            <p class="p-date" >2022-11-11</p>
+            <p class="p-user" >{articleValue.userEmail}</p>
+            <p class="p-date" >{articleValue.createdAt}</p>
         </div>
     </div>
 
     <div class="content-box-main">
-        <textarea id="message" rows="5" class="slog-content-textarea " placeholder="내용을 입력해 주세요."></textarea>
+        <textarea id="message" rows="5" class="slog-content-textarea " placeholder="내용을 입력해 주세요."
+        bind:value={articleValue.content}
+        ></textarea>
     </div>
 
     <div class="content-box-bottom">
